8b28ab2a9a4d4bcb3dace9693ced10c12bc4f01a
Regina Lionheart <regina@lionhe.art>
2020-01-10 14:37:16 -0800

    Create dummy dataset with slightly changed RT values.

--- FA_RetentionTime.R
+++ FA_RetentionTime.R
@@ -39,6 +39,11 @@ size.fraction_0.2 <- combined.expected %>%
 size.fraction_0.3 <- combined.expected %>%
   filter(!str_detect(Replicate.Name, "0.2")) 
 
+dummy.data <- size.fraction_0.2 %>%
+  select(Replicate.Name, Metabolite.Name, RT.Value, RT.Expected, Cmpd.with.Std) %>%
+  mutate(Random = RT.Value + sample(1, size = nrow(dummy.data), replace = TRUE))
+
+
 # First plot of retention time ----------------------------------------------------
 all.RT.plot <- ggplot(size.fraction_0.2, aes(x = Metabolite.Name, y = RT.Value, fill = Cmpd.with.Std)) +
   geom_bar(stat = "identity", position = "dodge") +
@@ -49,14 +54,18 @@ all.RT.plot <- ggplot(size.fraction_0.2, aes(x = Metabolite.Name, y = RT.Value, 
 ggtitle("Fatty Acids: 0.2 Size Fraction")
 print(all.RT.plot)
 
-all.Mz.plot <- ggplot(size.fraction_0.2, aes(x = Metabolite.Name, y = Mz.Value, color = Cmpd.with.Std)) +
-  geom_point(stat = "identity") +
+# And with dummy data
+dummy.plot <- ggplot(dummy.data, aes(x = Metabolite.Name, y = Random, fill = Cmpd.with.Std)) +
+  geom_bar(stat = "identity", position = "dodge") +
   theme(axis.text.x = element_text(angle = 90, size = 10),
         axis.text.y = element_text(size = 10),
         legend.position = "top",
         strip.text = element_text(size = 10)) +
-  ggtitle("Fatty Acids: 0.2 Size Fraction")
-print(all.Mz.plot)
+  ggtitle("Random RT Values")
+print(dummy.plot)
+
+require(gridExtra)
+grid.arrange(all.RT.plot, dummy.plot, nrow=2)
 
 ################################################################################
 # Retention Time Table ----------------------------------------------------
@@ -89,10 +98,10 @@ FA.18 <- RT.Table %>%
 FA.18 <- FindStdDev(FA.18) 
 FA.18 <- FA.18 %>% 
   mutate(Metabolite.Name = recode(Metabolite.Name,
-                                  `FA 18:0_Std` = "0",
-                                  `FA 18:1 cis-6_Std` = "1",
-                                  `FA 18:2_Std` = "2",
-                                  `FA 18:3_Std` = "3")) 
+                                  "FA 18:0_Std" = "0",
+                                  "FA 18:1 cis-6_Std"= "1",
+                                  "FA 18:2_Std" = "2",
+                                  "FA 18:3_Std" = "3")) 
 FA.18$Metabolite.Name <- as.numeric(as.character(FA.18$Metabolite.Name))
 
 # Separate Replicates
@@ -103,19 +112,70 @@ ggplot(FA.18, aes(fill=Replicate.Name, y=RT.Value, x=Metabolite.Name)) +
   ggtitle("Fatty Acid 18: 0-4")
 
 # Grouped Replicates with Regression
-ggplot(FA.18, aes(x = Metabolite.Name, y = Mean.RT.Value)) +
+FA.18.plot <- ggplot(FA.18, aes(x = Metabolite.Name, y = Mean.RT.Value)) +
+  geom_point(aes(color = "Averaged RT Value")) +
+  geom_point(aes(x = Metabolite.Name, y = RT.Expected, color = "Expected RT Value")) +
+  geom_smooth(method = "lm", fullrange = TRUE, color = "black", formula = y ~ x) +
+  ylim(0, 15) +
+  xlim(0, 5) +
+  theme(strip.text = element_text(size = 10)) +
+  ggtitle("Fatty Acid 18: 0-4")
+print(FA.18.plot)
+
+FA18.model <- lm(formula = Mean.RT.Value ~ Metabolite.Name, data = FA.18)
+FA18.fitted <- predict(FA18.model)
+FA18.resid <- residuals(FA18.model)
+
+myY <- (FA18.model$coefficients[2]*4) + FA18.model$coefficients[1]
+realY <- size.fraction_0.2 %>%
+  filter(Metabolite.Name == "FA 18:4") %>%
+  filter(str_detect(Replicate.Name, "_Std_")) %>%
+  filter(!str_detect(Replicate.Name, "IS")) %>%
+  summarize(mean(RT.Value))
+  
+
+
+################################################################################
+# FA 20 test ----------------------------------------------------
+FA.20 <- size.fraction_0.2 %>%
+  filter(str_detect(Metabolite.Name, "20")) %>%
+  filter(str_detect(Replicate.Name, "_Std_")) %>%
+  filter(!str_detect(Metabolite.Name, "IS")) %>%
+  group_by(Metabolite.Name) %>%
+  mutate(Mean.RT.Value = mean(RT.Value, na.rm = TRUE))
+
+FA.20 <- FindStdDev(FA.20) 
+
+FA.20 <- FA.20 %>% # 4 + 5 are standards
+  mutate(Metabolite.Name = recode(Metabolite.Name,
+                                  "FA 20:0" = "0",
+                                  "FA 20:1" = "1",
+                                  "FA 20:2" = "2",
+                                  "FA 20:3" = "3",
+                                  "FA 20:4_Std" = "4",
+                                  "FA 20:5_Std" = "5")) 
+FA.20$Metabolite.Name <- as.numeric(as.character(FA.20$Metabolite.Name))
+
+# Replicates included 
+ggplot(FA.20, aes(fill=Replicate.Name, y=RT.Value, x=Metabolite.Name)) + 
+  geom_bar(position="dodge", stat="identity") +
+  geom_errorbar(aes(ymin=RT.Value - Std.dev, ymax=RT.Value + Std.dev), width=.2,
+                position=position_dodge(.9)) +
+  ggtitle("Fatty Acid 20: 0-5")
+
+# Replicates grouped
+ggplot(FA.20, aes(x = Metabolite.Name, y = Mean.RT.Value)) +
   geom_point(aes(color = "Averaged RT Value")) +
   geom_point(aes(x = Metabolite.Name, y = RT.Expected, color = "Expected RT Value")) +
   geom_smooth(method = "lm", color = "black", formula = y ~ x) +
   ylim(0, 15) +
   xlim(0, 5) +
   theme(strip.text = element_text(size = 10)) +
-  ggtitle("Fatty Acid 18: 0-4")
+  ggtitle("Fatty Acid 20: 0-4")
 
-FA.model <- lm(formula = Mean.RT.Value ~ Metabolite.Name, data = FA.18)
-FA.model
-
-
+FA20.model <- lm(formula = Mean.RT.Value ~ Metabolite.Name, data = FA.20)
+FA20.fitted <- predict(FA20.model)
+FA20.resid <- residuals(FA20.model)
 ################################################################################
 # K means clustering ----------------------------------------------------
 ggplot(RT.Table, aes(RT.Value, Replicate.Name, color = Metabolite.Name)) + 
