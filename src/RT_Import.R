cd4ea9463ba31541901fe238dd27262b776530cc
Regina Lionheart <regina@lionhe.art>
2020-02-03 16:09:46 -0800

    First complete runthrough with Control.Rmd.

---
+++ src/RT_Import.R
@@ -0,0 +1,33 @@
+# Retention time prediction 
+
+pattern = "combined"
+
+# Import QC'd files ----------------------------------------------------
+filename <- RemoveCsv(list.files(path = "data_intermediate/", pattern = pattern))
+filepath <- file.path("data_intermediate", paste(filename, ".csv", sep = ""))
+
+fatty.acids <- assign(make.names(filename), read.csv(filepath, stringsAsFactors = FALSE, header = TRUE)) %>%
+  mutate(Run.Type = (tolower(str_extract(Replicate.Name, "(?<=_)[^_]+(?=_)")))) 
+
+# Create file isolating runs that have comments.
+file.comments <- fatty.acids %>%
+  select(Replicate.Name, Metabolite.Name, Comment) %>%
+  filter(!nchar(Comment) < 5)
+
+# Import expected retention time files ----------------------------------------------------
+FA.expected <- read.csv("data_extras/FA_Expected_RT.csv", stringsAsFactors = FALSE) %>%
+  rename(Metabolite.Name = Name) %>%
+  rename(RT.Expected = RT) %>%
+  select(Metabolite.Name, RT.Expected)
+  
+FA.expected <- fatty.acids %>%
+  left_join(FA.expected) %>%
+  mutate(Cmpd.with.Std = ifelse(str_detect(Metabolite.Name, "_Std"), "Standard.Compound", 
+                                ifelse(str_detect(Metabolite.Name, "IS"), "Internal.Standard", "NonStandard.Compound"))) %>%
+  group_by(Metabolite.Name) %>%
+  mutate(Mean.RT.Value = mean(RT.Value, na.rm = TRUE),
+         Min.RT.Value = min(RT.Value, na.rm = TRUE),
+         Max.RT.Value = max(RT.Value, na.rm = TRUE)) %>%
+  select(Replicate.Name, Metabolite.Name, RT.Value, RT.Expected, Mean.RT.Value:Max.RT.Value, Cmpd.with.Std) 
+
+rm(list = setdiff(ls()[!ls() %in% c("FA.expected")], lsf.str()))
